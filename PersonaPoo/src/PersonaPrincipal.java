c100427c8a555b4faa84b623231002403ce8c3f5
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 12:17:42 -0500

    Primer ejercicio Poo

---
+++ PersonaPoo/src/PersonaPrincipal.java
@@ -0,0 +1,15 @@
+
+public class PersonaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+PersonaProgramacionOrientadaAObjetos 
+Claudia = new PersonaProgramacionOrientadaAObjetos();
+		Claudia.name = "Claudia Flores ";
+		Claudia.age = 25;
+		System.out.println(Claudia.saludar());
+		
+
+	}
+
+}
---
+++ PersonaPoo/src/PersonaProgramacionOrientadaAObjetos.java
@@ -0,0 +1,10 @@
+
+public class PersonaProgramacionOrientadaAObjetos {
+	String name = "" ;
+	int age = 0;
+	
+	public String saludar() {
+		return " Hola, soy " + this.name + " Mi edad es " + this.age + " años";	
+	}
+	
+}
